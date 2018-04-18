3a5c78525675c54c172eea8cab222033bc655b87
sharon <sharonoptsout@gmail.com>
2018-04-18 21:53:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var recipes = {};
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({object},key,value);
+  return(object);
+}
+
 /*
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
