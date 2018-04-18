43304180bd72f11bdac75f8b67c5b034a5707ec9
sharon <sharonoptsout@gmail.com>
2018-04-18 21:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 var recipes = {};
 
 
-/*
+
 function updateObjectWithKeyAndValue(object, key, value) {
   Object.assign({object},key,value);
   return(object);
 }
-*/
-/*
-function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return(object);
-}
-*/
+
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
-  Object.assign({object},key,value);
+  object[key] = value;
   return(object);
 }
