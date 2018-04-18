b38f37cc082ef32305fa52bbe2ac79f440d360e6
sharon <sharonoptsout@gmail.com>
2018-04-18 21:56:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 */
 
-function destructivelyUpdateObjectWithKeyAndValue(key,value) {
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
   Object.assign({object},key,value);
   return(object);
 }
